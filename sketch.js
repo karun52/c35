70fc4e276cd2e930984fe1e9a686ca7713884bdd
karun52 <65775306+karun52@users.noreply.github.com>
2020-07-01 17:08:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canvas, backgroundImg,database;
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  canvas=createCanvas(400,400);
+
+
+  
+}
+
+function draw(){}
